fb47c8c7ae5a37c072edc838e9ffa99f3450f75e
Sheena Artrip <sheena.artrip@gmail.com>
2016-06-05 01:26:28 -0400

    urlshorten - scala-osgi - initial dummy REST bundle

---
+++ urlshorten-scala-osgi/net.sheenobu.urlshorten.rest/src/main/scala/Activator.scala
@@ -0,0 +1,11 @@
+package net.sheenobu.urlshorten.rest
+
+import org.osgi.framework.BundleContext
+import org.apache.felix.dm._
+
+class Activator extends DependencyActivatorBase {
+
+	def init(ctx: BundleContext, manager: DependencyManager) = ???
+	def destroy(ctx: BundleContext, manager: DependencyManager) = ???
+
+}
